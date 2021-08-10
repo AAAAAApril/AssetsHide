9700ea603f86171e64aba278f3ef8fbb08ef181d
wq <wq@qq.com>
2021-08-10 18:22:20 +0800

    '调整生成的代码模板'

--- lib/src/Templates.dart
+++ lib/src/Templates.dart
@@ -13,24 +13,20 @@ String generateAssetsDartFileContent({
 // generated code , do not change by yourself
 import 'dart:convert';
 
-// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
-// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
-// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
-
 String _fromIntList(List<int> value){
   return utf8.decode(value.map<int>((e) => ~e).toList());
 }
 
 ${assets.map((e) {
     return """
-class ${e.className}{
+    
+class ${e.className} {
   ${e.className}._();
 
 ${e.labels.map((e) {
       return e.generateDartField('_fromIntList', toIntList(e.value));
     }).join('\n')}
 }
-
 """;
   }).join('\n')}
 
